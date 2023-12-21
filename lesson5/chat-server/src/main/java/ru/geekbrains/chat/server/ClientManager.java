b33f247b27f0a4c0a9b3bc9ba19ae2fb4961c9d1
see123457 <151024510+see123457@users.noreply.github.com>
2023-12-21 20:00:45 +1000

    Add files via upload

---
+++ lesson5/chat-server/src/main/java/ru/geekbrains/chat/server/ClientManager.java
@@ -0,0 +1,112 @@
+package ru.geekbrains.chat.server;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientManager implements Runnable {
+
+    private final Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String name;
+
+    public final static ArrayList<ClientManager> clients = new ArrayList<>();
+
+    public ClientManager(Socket socket) {
+        this.socket = socket;
+        try {
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            name = bufferedReader.readLine();
+            clients.add(this);
+            System.out.println(name + " подключился к чату.");
+            broadcastMessage("Server: " + name + " подключился к чату.");
+        }
+        catch (IOException e){
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+
+
+    }
+
+    @Override
+    public void run() {
+        String massageFromClient;
+
+        while (socket.isConnected()) {
+            try {
+                massageFromClient = bufferedReader.readLine();
+                /*if (massageFromClient == null){
+                    // для  macOS
+                    closeEverything(socket, bufferedReader, bufferedWriter);
+                    break;
+                }*/
+                broadcastMessage(massageFromClient);
+            }
+            catch (IOException e){
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+
+    private void broadcastMessage(String message){
+        for (ClientManager client: clients) {
+            try {
+                if (!client.name.equals(name)) {
+                    if(message == null || message.isEmpty()){
+                        continue;
+                    }
+                    if (message.replace(name + ": ", "").startsWith("@")) {
+                        String[] splitMessage = message.replace(name + ": ", "").split(" ", 2);
+                        String recipient = splitMessage[0].substring(1);
+                        if(client.name.equals(recipient)) {
+                            client.bufferedWriter.write("[Лично вам] " + message);
+                            client.bufferedWriter.newLine();
+                            client.bufferedWriter.flush();
+                        }
+                    }
+                    else {
+                        client.bufferedWriter.write(message);
+                        client.bufferedWriter.newLine();
+                        client.bufferedWriter.flush();
+                    }
+                }
+
+            }
+            catch (IOException e){
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+    }
+
+
+    private void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        // Удаление клиента из коллекции
+        removeClient();
+        try {
+            // Завершаем работу буфера на чтение данных
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            // Завершаем работу буфера для записи данных
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            // Закрытие соединения с клиентским сокетом
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void removeClient(){
+        clients.remove(this);
+        System.out.println(name + " покинул чат.");
+        broadcastMessage("Server: " + name + " покинул чат.");
+    }
+
+}
